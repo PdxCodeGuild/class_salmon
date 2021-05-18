4c6571f20d75665d237e2f4ef1643d98f8556ce9
Brea <breabrown96@gmail.com>
2021-05-18 16:16:37 -0700

    second lab with comments

--- code/brea/lab_02.py
+++ code/brea/lab_02.py
@@ -1,19 +1,19 @@
-numbers = []
-print('Let\'s average some numbers!')
-while True:
-    user_input = input("Enter a number to add to the list, enter 'done' when finished: ").lower()
-    numbers.append(user_input)
-    if user_input == 'done':
-        break
-numbers.remove('done')  
+numbers = [] # defining a variable as a list
+print('Let\'s average some numbers!') # printing a string
+while True: # start of while loop for averaging numbers
+    user_input = input("Enter a number to add to the list, enter 'done' when finished: ").lower() # defining a variable and inputting data into list
+    numbers.append(user_input) # adding to the end of the list
+    if user_input == 'done': # if statement checking to see if input equals done
+        break # breaks loop
+numbers.remove('done')  # removes the word done from the list
 
-print('The list of numbers you entered was: ')
-for item in numbers:
-    print(item)
-sum = 0
+print('The list of numbers you entered was: ') # printing a string 
+for item in numbers: # for loop starting codeblock
+    print(item) # prints items of the list
+sum = 0 # defining a variable
 
-for item in numbers:
-   sum += int(item) 
-print(f'The sum of the list of numbers was: {sum} ')     
-avg = sum / len(numbers)
-print(f'The average of the list is: {avg}')
+for item in numbers: # for loop starting codeblock for averaging
+   sum += int(item)  # variable is added to the items and making items an integer
+print(f'The sum of the list of numbers was: {sum} ')  # printing the sum of the items in the list
+avg = sum / len(numbers) # generating the average
+print(f'The average of the list is: {avg}') # f string containing a statement and the average
