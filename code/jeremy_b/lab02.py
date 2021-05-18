650dfee118cd554a073d966f08f413692a42830d
Jeremy Bush <jeremy@b34systems.com>
2021-05-18 15:05:39 -0700

    Refactored to include 1 input string and average will display as int if no remainder or float rounded to 2 decimal places if there is.

--- code/jeremy_b/lab02.py
+++ code/jeremy_b/lab02.py
@@ -14,6 +14,8 @@ This code was used for version 1.
 nums = [3, 4, 5]
 """
 
+"""
+# Version 2 code for obtaining input.
 # Get user to input numbers.
 nums = []
 number_entered = ''
@@ -24,11 +26,28 @@ while True:
         break
     else:
         nums.append(int(number_entered))
+"""
+
+# Version 3 code
+# Let user input everything in one input statement.
+print("Enter integers to average.  Separate each with a comma.  Press enter when done.")
+numbers_entered = input("Enter integers: ")
+
+# find and replace any spaces in the string.
+numbers_entered = numbers_entered.replace(" ", "")
+
+# split string and convert values into integers.
+numbers_entered = numbers_entered.split(",")
+nums = [int(number) for number in numbers_entered]  # Conversion via list comprehension.
+
 
 # get the total sum of all values.
 total = 0
 for number in nums:
     total += number
 
-# print the average of the numbers in the list.
-print(f"Average: {int(total/len(nums))}")
+# print the average of the numbers in the list.  If average has no remainder, print as int.
+if total % len(nums) == 0:
+    print(f"Average: {int(total/len(nums))}")
+else:
+    print(f"Average: {total/len(nums):.2f}")
