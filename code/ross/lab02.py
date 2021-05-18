588e31c644c230d8421b822a6a6fe3d26bb316bc
Ross Zeiger <rosszeiger1@gmail.com>
2021-05-18 12:22:14 -0700

    lab02

--- code/ross/lab02.py
+++ code/ross/lab02.py
@@ -1,7 +1,14 @@
-nums = [5, 0, 8, 3, 4, 1, 6]
+user_input = input("Please enter a number, or 'done': ")
+nums = []
 total = 0
 
+# this section adds numbers to the list until a user tells it they're done
+while user_input != 'done':
+    nums.append(int(user_input))
+    user_input = input("Please enter a number, or 'done': ")
+
+# this creates a sum of all the entered numbers and stores it in a variable called 'total'
 for num in nums:
     total += num
 
-print(round(total / len(nums), 2))
+print(f"You entered {len(nums)} numbers and the average of your numbers is: {total / len(nums)}")
