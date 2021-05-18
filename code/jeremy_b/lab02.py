1175d168617c2d2ce8bb6db9012f4ca6c8042512
Jeremy Bush <jeremy@b34systems.com>
2021-05-18 14:01:51 -0700

    lab 2 version 2 commit

--- code/jeremy_b/lab02.py
+++ code/jeremy_b/lab02.py
@@ -2,13 +2,28 @@
 
 Author: Jeremy Bush
 Project: Programming Boot Camp Lab 02
-Version: 1
+Version: 2
 Date: 5/18/2021
 
 """
 
+"""
+This code was used for version 1.
+
 # List of numbers
 nums = [3, 4, 5]
+"""
+
+# Get user to input numbers.
+nums = []
+number_entered = ''
+print("Enter integers to average.  Once numbers have been entered, enter 'done'.")
+while True:
+    number_entered = input("Enter an integer: ")
+    if number_entered.lower() == 'done':
+        break
+    else:
+        nums.append(int(number_entered))
 
 # get the total sum of all values.
 total = 0
