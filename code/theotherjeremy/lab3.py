7c2878d9ddbdc4c22897a64d6d4fbd66f46c29d7
Jeremy <82420963+LambsDamnPrograms@users.noreply.github.com>
2021-05-19 14:52:20 -0700

    Lab 3 version 2.ugly

--- code/theotherjeremy/lab3.py
+++ code/theotherjeremy/lab3.py
@@ -1,8 +1,15 @@
 x = int(input('Gimme a number from 0 - 99:  '))
-tens = x//10
-ones = x % 10
 
-ones_dict = {0: 'zero',
+hunds = x//100
+
+tens = (x % 100) // 10
+
+ones = x % 10
+if x > 1 and ones == 0:
+    ones = ''
+
+ones_dict = {'': '',
+             0: 'zero',
              1: 'one',
              2: 'two',
              3: 'three',
@@ -24,7 +31,8 @@ ones_dict = {0: 'zero',
              19: 'nineteen',
              }
 
-tens_dict = {2: 'twenty',
+tens_dict = {0: '',
+             2: 'twenty',
              3: 'thirty',
              4: 'forty',
              5: 'fifty',
@@ -34,17 +42,15 @@ tens_dict = {2: 'twenty',
              9: 'ninety'}
 
 
-if tens < 2:  # or ones == 0:
+if tens < 2 and x < 20:
     print(ones_dict[x])
 
 else:
-    if ones == 0:
+    if ones == 0 and x < 100:
         print(tens_dict[tens])
     else:
-        print(f'{tens_dict[tens]}{ones_dict[ones]}')
-
-# elif tens < 2 or ones == 0:
-#    ones in ones_dict:
-#    print(ones_dict[ones])
-# print(tens)
-# print(ones)
+        if x < 99:
+            print(f'{tens_dict[tens]} {ones_dict[ones]}')
+        else:
+            print(
+                f'{ones_dict[hunds]} hundred {tens_dict[tens]} {ones_dict[ones]}')
