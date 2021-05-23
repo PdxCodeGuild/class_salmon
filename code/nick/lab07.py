a7bdae5680857668daf5ba17ebdd3661c6efee37
Nick Fry <nicholas18@ru.is>
2021-05-23 22:50:13 +0000

    lab07 modification

--- code/nick/lab07.py
+++ code/nick/lab07.py
@@ -6,11 +6,11 @@ print(list_user_string)
 ROT13_dict = {'a':'n', 'b':'o', 'c':'p', 'd':'q', 'e':'r', 'f':'s', 'g':'t', 'h':'u', 'i':'v', 'j':'w', 
 'k':'x', 'l':'y', 'm':'z', 'n':'a', 'o':'b', 'p':'c', 'q':'d', 'r':'e', 's':'f', 't':'g', 'u':'h', 'v':'i', 'w':'j', 
 'x':'k', 'y':'l', 'z':'m'}
-encrypted_string = []
+encoding_string = []
 for i in range(len(list_user_string)):
-    encrypted_string.append(ROT13_dict[list_user_string[i]])
+    encoding_string.append(ROT13_dict[list_user_string[i]])
     #remember to put the i in the nested list above.
-    print(encrypted_string)
+    print(encoding_string)
 #I am not sure this is what the class intent was, but it is my first attempt.
 
 
