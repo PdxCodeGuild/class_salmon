b5e76e03e69c71d068820c8e67303d084319a8a3
K C <klsycanoy@yahoo.com>
2021-05-23 22:53:49 -0700

    lab 05-07 in progress

--- code/kelsey/lab05.py
+++ code/kelsey/lab05.py
@@ -26,7 +26,7 @@ expenses = 0
 earnings = 0
 
 # Calculate your net winnings (the sum of all expenses and earnings).
-while counter <= 5:
+while counter < 10:
     counter += 1
     expenses += 2
     balance = earnings - expenses
