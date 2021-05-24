7518e3862ea7af25089cb1c321a410757bffb0bb
b34systems <jeremy@b34systems.com>
2021-05-24 12:18:34 -0700

    switching typist update

--- code/brea/group_project1.py
+++ code/brea/group_project1.py
@@ -7,10 +7,37 @@ Austin
 '''
 
 
-year_counter = 0
+year = 0
 population = [0, 0]
 total_population = len(population)
 
-def populate(popualtion):
+
+def populate():
+    global year, population
+
     while len(population) < 1000:
-        population = [(if 1 == 10 population.pop(i) else i += 1) for i in population]
+        year += 1
+        pairs = 0
+        # population = [(population.remove(i) if i == 10 else i + 1) for i in population]
+        for i in population:
+            if i != 10:
+                i += 1
+            else:
+                population.remove(i)
+
+
+
+        mating_age = 0
+        for jackelope in population:
+            if 4 <= jackelope <= 8:
+                mating_age += 1
+        pairs = mating_age // 2
+
+        while pairs > 0:
+            population.append(0)
+            population.append(0)
+            pairs -= 1
+
+    return year
+
+print(populate())
