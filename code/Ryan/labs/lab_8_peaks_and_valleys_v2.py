0b1f0b75ddf89e9beaec142b9ae46ac664ca797b
Ryan Woolsey <ryanleewoolsey@gmail.com>
2021-05-25 00:34:59 -0700

    Scraped original lab due to script not running in VS code.
    Remade lab as V2.
    Able to spot changes in direction.
    Need to identify how to extract the index position of the change and
    push the index number to peak/valley function.

--- code/Ryan/labs/lab_8_peaks_and_valleys_v2.py
+++ code/Ryan/labs/lab_8_peaks_and_valleys_v2.py
@@ -287,17 +287,186 @@ elif i_1 - i_0 == -1:
                                                                                     print(f"continue {direction}")
 """
 
+"""
 thislist = ["apple", "banana", "cherry"]
 for i in range(len(thislist)):
   print(thislist[i]) # apple banana cherry
+"""
 
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-for i in range(len(data)):
-  print(str(data[i])) # SyntaxError: invalid syntax
-"""
-"""
->>> & C:/Users/aznkr/AppData/Local/Programs/Python/Python39/python.exe c:/Users/aznkr/Desktop/pdx_code/class_salmon/code/Ryan/labs/lab_8_peaks_and_valleys.py
-  File "<stdin>", line 1
-    & C:/Users/aznkr/AppData/Local/Programs/Python/Python39/python.exe c:/Users/aznkr/Desktop/pdx_code/class_salmon/code/Ryan/labs/lab_8_peaks_and_valleys.py
-"""
+indexes = [i for i in range(len(data))]
+# print(indexes)
+# print(data[indexes]) #TypeError: list indices must be integers or slices, not list
+data_dict = {
+0: data[0],
+1: data[1],
+2: data[2],
+3: data[3],
+4: data[4],
+5: data[5],
+6: data[6],
+7: data[7],
+8: data[8],
+9: data[9],
+10: data[10],
+11: data[11],
+12: data[12],
+13: data[13],
+14: data[14],
+15: data[15],
+16: data[16],
+17: data[17],
+18: data[18],
+19: data[19],
+20: data[20]
+}
+
 print(data)
+
+
+"""
+for each_key in data_dict:
+    #print(each_key)
+    current_key_value = data_dict.get(each_key)
+    next_key_value = data_dict.get(each_key+1)
+    #print(next_key_value - current_key_value)
+    #result = next_key_value - current_key_value # TypeError: unsupported operand type(s) for -: 'NoneType' and 'int'
+    #print(type(result)) # <class 'int'>
+    #results_list += int(result) #TypeError: 'int' object is not iterable
+    #result += results_list #TypeError: unsupported operand type(s) for +=: 'int' and 'list'
+    #past_result = result
+
+#past_result = int()
+#print(past_result) #TypeError: unsupported operand type(s) for -: 'NoneType' and 'int'
+"""
+"""
+num_1 = int()
+num_2 = int()
+
+start = True
+while start:
+    for x in input("Enter a number: "):
+        print(x)
+        if num_1 == False:
+            num_1 = x
+            print(f"Adding {x} to num_1")
+        elif num_1 == True and num_2 == False:
+            num_2 = x 
+            print(f"Adding {x} to num_2")
+        elif num_1 == True and num_2 == True:
+            current_direction = num_2 - num_1
+            if current_direction == 1:
+                current_direction = "Going up"
+            elif current_direction == -1:
+                current_direction = "Going down"
+            print(f"Subtracting {num_1} from {num_2}. {current_direction} ")
+"""
+
+"""
+for each_key in data_dict:
+    #print(each_key)
+    previous_key_value = data_dict.get(each_key-1)
+    current_key_value = data_dict.get(each_key)
+    next_key_value = data_dict.get(each_key+1)
+
+    if previous_key_value == False:
+        continue
+
+    if next_key_value == False:
+        continue
+
+    def peak(data):
+        if previous_key_value < current_key_value < next_key_value: #TypeError: '<' not supported between instances of 'NoneType' and 'int'
+            direction = "uphill"
+        return direction 
+
+    print(peak(each_key))
+
+
+print(data_dict.values())
+
+for x in data_dict:
+    print(f"{data_dict.keys()} {data_dict.values()}")
+"""
+
+# For each item in the list
+# Find the difference
+#for x in data:
+    #data = data[x+1] #TypeError: 'int' object is not subscriptable
+
+result_list = []
+
+
+diff_1 = data[1] - data[0]
+diff_2 = data[2] - data[1]
+diff_3 = data[3] - data[2]
+diff_4 = data[4] - data[3]
+diff_5 = data[5] - data[4]
+diff_6 = data[6] - data[5]
+diff_7 = data[7] - data[6]
+diff_8 = data[8] - data[7]
+diff_9 = data[9] - data[8]
+diff_10 = data[10] - data[9]
+diff_11 = data[11] - data[10]
+diff_12 = data[12] - data[11]
+diff_13 = data[13] - data[12]
+diff_14 = data[14] - data[13]
+diff_15 = data[15] - data[14]
+diff_16 = data[16] - data[15]
+diff_17 = data[17] - data[16]
+diff_18 = data[18] - data[17]
+diff_19 = data[19] - data[18]
+diff_20 = data[20] - data[19]
+
+
+
+
+result_list.append(diff_1)
+result_list.append(diff_2)
+result_list.append(diff_3)
+result_list.append(diff_4)
+result_list.append(diff_5)
+result_list.append(diff_6)
+result_list.append(diff_7)
+result_list.append(diff_8)
+result_list.append(diff_9)
+result_list.append(diff_10)
+result_list.append(diff_11)
+result_list.append(diff_12)
+result_list.append(diff_13)
+result_list.append(diff_14)
+result_list.append(diff_15)
+result_list.append(diff_16)
+result_list.append(diff_17)
+result_list.append(diff_18)
+result_list.append(diff_19)
+result_list.append(diff_20)
+
+print(result_list)
+
+def peaks(data):
+    return
+
+def valleys(data):
+    return
+
+def peaks_and_valleys(peaks, valleys):
+    return
+
+start = 0
+running_total = 0
+
+for each_difference in result_list:
+    running_total += each_difference
+    print(f"Start: {result_list(each_difference)}")
+    print(f"Running total: {running_total}")
+
+    if start < running_total:
+        print("Going up")
+        running_total = 1 
+    elif start > running_total:
+        print("Going down")
+        running_total = -1
+    elif start == running_total:
+        print("Direction change")
+        running_total = 0
