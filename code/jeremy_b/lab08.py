b62dd8cb81eaa74fd950802ab25f98a54e339aa1
b34systems <jeremy@b34systems.com>
2021-05-24 22:20:46 -0700

    Function to find peaks now works.  Not so much with valleys.

--- code/jeremy_b/lab08.py
+++ code/jeremy_b/lab08.py
@@ -8,16 +8,26 @@ Date: 5/24/2021
 """
 
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-peaks = []
-valleys = []
-counter = 0
 
-while counter != len(data):
-    if counter > 0:
-        if data[counter] > data[counter - 1] and data[counter] > data[counter + 1]:
-            peaks.append(data[counter])
-        elif data[counter] < data[counter - 1] and data[counter] < data[counter + 1]:
-            valleys.append(data[counter])
-        counter += 1
-    else:
-        counter += 1
+
+def peaks(data):
+    peaks = []
+    counter = 0
+    while counter < len(data):
+        if counter == 0:
+            counter += 1
+        elif counter == (len(data) - 1):
+            counter += 1
+        else:
+            if data[counter] > data[counter - 1] and data[counter] > data[counter + 1]:
+                peaks.append(data.index(data[counter]))
+            counter += 1
+    return peaks
+
+
+def valleys(data):
+
+
+
+print(peaks(data))
+print(valleys(data))
