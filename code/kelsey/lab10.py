b30b0d0f019b7e6e7679bb3f15baee785f535f82
K C <klsycanoy@yahoo.com>
2021-05-27 16:26:52 -0700

    lab10 in progress

--- code/kelsey/lab10.py
+++ code/kelsey/lab10.py
@@ -22,25 +22,37 @@ print(list_of_dicts)
 #Version 2 -Implement a CRUD REPL
 
 # Create a record: ask the user for each attribute, add a new contact to your contact list with the attributes that the user entered.
-def create_record():
+def created_record():
     enter_name = input('Enter your name: ')
     enter_fav_animal = input('Enter your favorite animal: ')
     enter_fav_color = input('Enter your favorite color: ')
-    create_dict = {}
+    created_dict = {'name': enter_name, 'favorite animal': enter_fav_animal, 'favorite color': enter_fav_color}
+    return list_of_dicts.append(created_dict)
 
 
 # Retrieve a record: ask the user for the contact's name, find the user with the given name, and display their information
 def retrieve_record():
-
+    retrieve_name = input("What is the contact's name? ")
+    return individual_info[]
 # Update a record: ask the user for the contact's name, then for which attribute of the user they'd like to update and the value of the attribute they'd like to set.
-def update_record():
+def update_record(retrieve_record):
+    retrieve_record()
+
 
 # Delete a record: ask the user for the contact's name, remove the contact with the given name from the contact list.
-def delete_record():
+def delete_record(retrieve_record):
+    retrieve_record()
+    remove_record = input('Whose record would you like to remove? ')
+    return
 
 # Version 3 - write the updated contact info to the CSV file to be saved
 with open('contacts_copy.csv', 'w') as f:
     f.write(','.join(headers))
-    for i in list_of_dicts:
-       f.write()
+    for contact in list_of_dicts:
+        f.write('\n')
+        for header in headers:
+            if header != 'name':
+                f.write(',')
+            f.write(contact[header])
+        
 
