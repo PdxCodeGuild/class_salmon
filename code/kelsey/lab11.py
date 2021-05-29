5f9a764f3406df6be2b0ae2ac4d80138d74d5109
K C <klsycanoy@yahoo.com>
2021-05-28 17:28:38 -0700

    lab11 in progress

--- code/kelsey/lab11.py
+++ code/kelsey/lab11.py
@@ -10,37 +10,50 @@ def linear_search(nums, value):
             return index
     return -1    
 
-
-nums = [1, 2, 3, 4, 5, 6, 7, 8]
-index = linear_search(nums, 8)
-print(index) # 7
-
-
 # Part 2 - Binary Search
 # Implement binary search, which requires that a list is sorted and divides its search range in half each iteration until it finds its target.
-def binary_search(nums, value):
-    low = nums[0]
-    high = nums[-1]
-    while low < high:
-        mid = math.floor((low + high) / 2)
-        if nums[mid] < value:
-            low = mid + 1
-        elif nums[mid] > value:
-            high = mid - 1
+# def binary_search(nums, value):
+#     low = nums[0]
+#     high = nums[-1]
+#     while low <= high:
+#         mid = math.floor((low + high) / 2)
+#         if nums[mid] < value:
+#             low = mid + 1
+#             print(mid, low)
+#         elif nums[mid] > value:
+#             high = mid -1
+#             print(mid, high)
+#         else:
+#             return mid
+#     return -1
+
+def binary_search(A, T):
+    L = A[0]
+    R = A[-1]
+    while L <= R :
+        m = math.floor((L + R) / 2)
+        if A[m] < T :
+            L = m + 1
+        elif A[m] > T :
+            R = m - 1
         else:
-            return mid
+            return m
     return -1
 
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-index = binary_search(nums, 7) # returns -1 for values of 1 and 2
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+index = binary_search(nums, 1) # returns -1 for values of 1 and 2
 print(index) 
 
 
 # Part 3 - Bubble Sort
 # Bubble sort is one of the simplest and least efficient sorting algorithms. We repeatedly loop over the list, comparing each number to the one next to it, and swapping them if needed.
-def bubble_sort(nums):
-    length = len(nums)
-
+# def bubble_sort(nums):
+#     len(nums)
+#     for i, num in enumerate(nums): 
+#         swapped = False
+#         for i in nums:
+#             if 
+        
 #     procedure bubbleSort(A : list of sortable items)
 #     n := length(A)
 #     repeat
