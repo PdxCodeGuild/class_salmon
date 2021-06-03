254c7ae4688905f1bce17044503316c41182d6b5
Nick Fry <nicholas.a.fry@gmail.com>
2021-06-03 18:35:59 +0000

    lab14 version2 update

--- code/nick/P1Capstone.py
+++ code/nick/P1Capstone.py
@@ -4,10 +4,8 @@ while True:
     response = requests.get(f'https://api.openei.org/utility_rates?version=3&format=json&api_key={api_key}')
     print(response.text) #JSON format
     openenergyinformation_dict = response.json()#this is the joke dictionary, key is 'joke'
-    elif api_key == 'exit':
-        break
-    else:
-        print('Command not recognized')
+    
+
 
 # while True:
 #     command = input('What is your API key?')
