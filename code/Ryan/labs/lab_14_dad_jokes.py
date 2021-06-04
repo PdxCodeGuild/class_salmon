4df6420eef8c362f6fdb81d13d9d0886510f3cbc
Ryan Woolsey <ryanleewoolsey@gmail.com>
2021-06-04 15:34:21 -0700

    redo lab 6

--- code/Ryan/labs/lab_14_dad_jokes.py
+++ code/Ryan/labs/lab_14_dad_jokes.py
@@ -1,25 +1,7 @@
 import requests
 import json
-url = 'https://icanhazdadjoke.com/'
-payload = {'some': 'data'}
-headers = {'content-type': 'application/json'}
- 
-r = requests.post(url, data=json.dumps(payload), headers=headers)
 
-r = requests.get('https://icanhazdadjoke.com/')
-print(r.status_code)
-print(r.status_code == requests.codes.ok)
-print(requests.codes["temporary_redirect"])
-print(requests.codes.teapot)
-print(requests.codes["o/"])
-
-# HTML / CSS output
-# print(r.text) 
-print(r.json)
-
-response = requests.get('https://api.ipify.org')
-print(response.url)
-print(response.text) # 76.105.187.182
-print(response.status_code) # 200
-print(response.encoding) # ISO-8859-1
-print(response.headers) # {'Content-Type': 'text/plain', 'Content-Length': '14', ...}
+url = 'https://icanhazdadjoke.com/j/R7UfaahVfFd'
+response = requests.get(url)
+print(response.status_code)
+print(response.text)
