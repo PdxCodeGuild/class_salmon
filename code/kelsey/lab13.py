0d16e2d5c324ed3db6c578eecbbc0023858b0395
K C <klsycanoy@yahoo.com>
2021-06-03 20:41:50 -0700

    lab13

--- code/kelsey/lab13.py
+++ code/kelsey/lab13.py
@@ -62,28 +62,34 @@ class Game:
 
 def main():
     game = Game()
-    player_1 = Player((input(f'Enter Player 1 name: ')), 'X')
+    player_1 = Player((input(f'\nEnter Player 1 name: ')), 'X')
     player_2 = Player((input(f'Enter Player 2 name: ')), 'O')
     players = [player_1, player_2]
-    print('Start New Game')
+    print('\nStart New Game')
     counter = 0
     while game.is_game_over(player_1, player_2) == False:
         player = players[counter % 2]
-        print(f'{player}, your move')
-        valid_move = game.move(int(input('(from top left) move right _ space(s): ')), int(input('(from top left) move down _ space(s): ')), player)
+        print(f'\n{player}\'s turn \n')
+        valid_move = game.move(int(input('from far left, move right (0,1,2) space(s): ')), int(input('from top, move down (0,1,2) space(s): ')), player)
         if valid_move == True:
             counter += 1
         else:
             print('\nNot a valid move. Please choose again.\n')
-        print(repr(game))
+        print(f'\n{repr(game)}')
+        if game.is_full() == True and game.calc_winner(player) == True:
+            print(f'\nGame over. {player} wins!')
+            break
+        elif game.is_full() == True:
+            print(f'\nDraw. Game over.\n')
+            break
     for player in players:
         if game.calc_winner(player) == True:
-            print(f'{player} wins!')
+            print(f'\n{player} wins!\n')
     play_again = input('Would you like to play again? Y/N ').upper()
     if play_again == 'Y':
         main()
     else:
-        print('Thanks for playing!')
+        print('\nThanks for playing!\n')
         
 
 main()
