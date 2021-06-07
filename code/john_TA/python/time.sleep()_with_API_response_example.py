5e3f137da7e1303e9a975eeef2fbd90c6200af28
johnfial <12399190+johnfial@users.noreply.github.com>
2021-06-07 09:41:48 -0700

    example

---
+++ code/john_TA/python/time.sleep()_with_API_response_example.py
@@ -0,0 +1,17 @@
+import time
+
+# print('before')
+# time.sleep(1)
+# print('after')
+
+string = 'the quick brown ? ... fox'
+string_a = string.split('?')
+print(type(string_a))
+print(len(string_a))
+print(string_a[0])
+# string_b = 
+
+# print(string_a)
+time.sleep(2)
+print(string_a[1])
+{'joke': 'aoeuaoeu', 'punchline':'aoeu'} # we could do this if the returned joke was in this format, but it's not. Not really much we can do...
