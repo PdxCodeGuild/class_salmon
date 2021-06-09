744da10c3ee979872d16b67e6781588698b58bcf
Ryan Woolsey <ryanleewoolsey@gmail.com>
2021-06-08 22:58:03 -0700

    git update

---
+++ code/Ryan/labs/lab_16_mini_capstone/lab_16_mini_capstone.py
@@ -0,0 +1,11 @@
+#-----------------------------------------------------------------------------#
+#  Pygal needs to be installed first.
+
+    # Install command for Mac or Linux
+    # pip install --user pygal
+
+    # Install command for Windows
+    # python -m pip install --user pygal
+
+    # For more information visit: http://www.pygal.org/en/stable/#
+#-----------------------------------------------------------------------------#
