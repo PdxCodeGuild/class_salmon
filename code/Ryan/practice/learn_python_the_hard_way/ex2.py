a21d6b520c847d42b212f4fd023482eafe5ded3b
Ryan Woolsey <ryanleewoolsey@gmail.com>
2021-06-09 01:23:14 -0700

    adding lpthw exercises

---
+++ code/Ryan/practice/learn_python_the_hard_way/ex2.py
@@ -0,0 +1,15 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+# Study Drills
+# 1.Find out if you were right about what the # character does and make sure you know what it's called (octothorpe or pound character).
+# 2.Take your ex2.py file and review each line going backward. Start at the last line, and check each word in reverse against what you should have typed.
+# 3.Did you find more mistakes? Fix them.
+# 4.Read what you typed above out loud, including saying each character by its name. Did you find more mistake? Fix them 
---
+++ code/Ryan/practice/learn_python_the_hard_way/ex3_2.py
@@ -0,0 +1,3 @@
+# Print statement.
+print("How many total fish do I have?")
+print(11 + 3 + 2 + 1 + 19)
