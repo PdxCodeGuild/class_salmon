44ad267e8f69b981859ad6bc1c5c340771ce51d7
Jeremy <82420963+LambsDamnPrograms@users.noreply.github.com>
2021-06-09 22:58:47 -0700

    more progress

--- code/theotherjeremy/pythoncap2.py
+++ code/theotherjeremy/pythoncap2.py
@@ -1,10 +1,15 @@
 # ______________________- to veryify
 import requests
+import json
 data = {'grant_type': 'client_credentials', 'scope': 'product.compact'}
 headers = {'accept': 'application/json', 'Authorization': 'Basic Y2hlY2tlci05YmNmMGVkYzc1OTQ0YjI2MGRlODI3OWQ3YTZkODE3NDYwNjcxNzAxMzE5NTQzNDIyMTY6Qm9zTWRlWXVHS2NRSm95T0Y1SnladVJlbG9pY2g2MGtldHZ3Q3Z0Ug==',
            'Content-Type': 'application/x-www-form-urlencoded'}
 response = requests.post(
-    'https://api-ce.kroger.com/v1/connect/oauth2/token', headers=headers, data=data)
+    'https://api-ce.kroger.com/v1/connect/oauth2/token', headers=headers, data=data).json()
 response2 = response.text
+print(response2)
+official = response2['access_token']
+print(official)
 
-print(response2['access_token'])
+# output secrets.py
+# has 2 lines - secret = secrets
