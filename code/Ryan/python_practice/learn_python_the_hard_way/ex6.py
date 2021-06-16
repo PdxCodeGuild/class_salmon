377157167d9482ca7c0dbfc906f8b047973d51d8
Ryan Woolsey <ryanleewoolsey@gmail.com>
2021-06-16 10:34:29 -0700

    blog upload

--- code/Ryan/python_practice/learn_python_the_hard_way/ex6.py
+++ code/Ryan/python_practice/learn_python_the_hard_way/ex6.py
@@ -1,13 +1,19 @@
+# Make a variable and make it contain the integer of 10
 types_of_people = 10
+# Insert a formatted string that contains another variable into a variable named x.
 x = f"There are {types_of_people} types of people."
 
+# Create 2 string variables
 binary = "binary"
 do_not = "don't"
+# Make a variable named 'y' contain a formatted string with 2 string variables inside
 y = f"Those who know {binary} and those who {do_not}."
 
+# Print x and y variables
 print(x)
 print(y)
 
+# print x and y variables as a formatted string.
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
 
