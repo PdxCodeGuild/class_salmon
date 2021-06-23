0628c5a9163d78fa2d343edbb8f10292ecd4a014
code610 <82845353+code610@users.noreply.github.com>
2021-06-23 11:51:38 -0700

    group project update

--- code/nick/group_lab/04_todo_app.py
+++ code/nick/group_lab/04_todo_app.py
@@ -5,8 +5,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == "POST":
-        # any operations you want with request.form    
-        return render_template('04_todo.html')
+        # any operations you want with request.form
+        db = JsonDB('db.json')
+        db.load()
+        list_of_dicts = db.data['todos']
+        add_to_dict = {"text":request.form["text"],"priority":request.form["priority"]}
+        #list_of_dicts = db.get('todos',0)
+        list_of_dicts.append(add_to_dict)
+        print(list_of_dicts)
+        db.set("todos",list_of_dicts)
+        db.save()
+        return render_template('04_todo.html', list_of_dicts=list_of_dicts)
     else:
         db = JsonDB('db.json')
         db.load()
