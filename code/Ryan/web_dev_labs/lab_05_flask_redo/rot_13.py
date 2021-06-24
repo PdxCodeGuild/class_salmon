479630cf997df067afe8341a3957edb2a0f24a89
Ryan Woolsey <ryanleewoolsey@gmail.com>
2021-06-23 17:56:12 -0700

    flask redo

--- code/Ryan/web_dev_labs/lab_05_flask_redo/rot_13.py
+++ code/Ryan/web_dev_labs/lab_05_flask_redo/rot_13.py
@@ -6,23 +6,30 @@ def rot13(phrase, rotation):
     # out_phrase is an empty string
     out_phrase = ""
 
-    # For each letter in the phrase
-    for char in phrase:
+    # How many times to rotate
+    rotation = int(rotation)
+    #print(rotation)
 
-        # 1) In the abc list, find the letters original index
-        # 2) Add 13 to the index number
-        # 3) To ensure rotation after z, use modulus 26, which will give the remainder (index after rotation)
-        # 4) Append each letter to the string 
-        out_phrase += abc[(abc.find(char)+rotation)%26]
+    # Separate the words by spaces and make lower case
+    phrase = list(map(str, phrase.lower().split()))
+    #print(phrase)
 
-    return out_phrase
+    # Combine the words back into a list
+    for word in phrase:
 
-phrase = list(map(str, input().lower().split()))
-rotation = 20
+        # For each letter in the word
+        for char in word:
+            # 1) In the abc list, find the letters original index
+            # 2) Add 13 to the index number
+            # 3) To ensure rotation after z, use modulus 26, which will give the remainder (index after rotation)
+            # 4) Append each letter to the word 
+            out_phrase += abc[(abc.find(char)+rotation)%26]
 
-print(phrase)
+        out_phrase += " "
 
-for p in phrase:
-    print(rot13(p, rotation))
 
-#print(rot13(phrase, rotation))
+    return out_phrase   
+
+
+
+#print(rot13("this is a test", 10))
