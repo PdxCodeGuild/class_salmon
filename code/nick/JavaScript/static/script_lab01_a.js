a3cb47ad86dd1f96336cee0f4ea015d205123417
Nick Fry <nicholas.a.fry@gmail.com>
2021-06-29 22:55:06 +0000

    lab01_a for python lab 2

---
+++ code/nick/JavaScript/static/script_lab01_a.js
@@ -0,0 +1,28 @@
+//This is a JS version of lab2 python; Average numbers
+// nums = [5, 0, 8, 3, 4, 1, 6]
+// list = []
+// boolean = True
+// while boolean == True:
+//     num_or_done = input("Enter a number or 'done'")
+//     if num_or_done.isnumeric():
+//         list.append(float(num_or_done))
+//     elif num_or_done == 'done':
+//         boolean = False
+//         break
+
+// average_from_user = sum(list)/len(list)
+// print(f'The average of your list values are {average_from_user}.')
+
+let nums = [5, 0, 8, 3, 4, 1, 6]
+let list = []
+let boolean = true
+while (boolean === true) {
+    let num_or_done = prompt("Enter a number or 'done'")
+    if (function isNumeric(num_or_done) {
+        return !isNaN(parseFloat(num_or_done)) //This is supposed to mean is not not a number for the parsefloat of the input https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+}
+    }
+        
+
+
+alert(`the answer is ${a+b}`)
