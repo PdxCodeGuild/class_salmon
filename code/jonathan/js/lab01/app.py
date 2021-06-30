5c5a3124cc6da93d6c86eb5b2ac5c8b1f0cec9f0
Jonathan Spannring <jonathan.spannring@gmail.com>
2021-06-30 14:05:03 -0700

    js labs

--- code/jonathan/js/lab01/app.py
+++ code/jonathan/js/lab01/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 app.run(debug=True)
