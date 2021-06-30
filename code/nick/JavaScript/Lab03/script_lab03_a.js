dfbb47b36c7665b5b272a9addca899ddc98835b2
Nick Fry <nicholas.a.fry@gmail.com>
2021-06-30 21:18:30 +0000

    lab3 A complete

--- code/nick/JavaScript/Lab03/script_lab03_a.js
+++ code/nick/JavaScript/Lab03/script_lab03_a.js
@@ -43,22 +43,50 @@
 // result = list(peaks_and_valleys(peak_indices, valley_indices))
 // print(result) #I am not sure why this is appearing as ordered pairs but it is working otherwise.
 let data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-let peak_indices = []
+
 // console.log(data)
 function peaks(data) {
-    for (i in range(data[0], data[length(data)])) {
-        if ( i === 0 || i === ((length(data))-1)) {
+    let peak_indices = []
+    for (let i=0; i<data.length; ++i) {
+        if ( i === 0 || i === ((data.length)-1)) {
+            // console.log("firstloop")
             continue
         }
         // #won't let me start on the list at 0 or -1 position; added above if statement
-        else if (data[i-1] < data[i] > data[i+1]) {
+        else if (data[i-1] < data[i] && data[i] > data[i+1]) {
+            // console.log(".")
             peak_indices.push(i)
         }
-        else if (data[i-1] > data[i] <data[i+1]) {
+        else if (data[i-1] > data[i] && data[i] < data[i+1]) {
             continue
         }
-    return peak_indices
     }
+    return peak_indices //make sure the return is outside of the for loop!!
 }
-console.log(peak_indices)
-// peak_indices = peaks(data)
+
+let peak_indices = peaks(data)
+
+
+function valleys(data) {
+    let valleys_indices = []
+    for (let i=0; i < data.length; ++i) {
+        if (i === 0 || i === ((data.length)-1)) { //gave me "length is not a function" error
+            continue
+        } else if (data[i-1] > data[i] && data[i] < data[i+1]) {
+            valleys_indices.push(i)
+        } else if (data[i-1] < data[i] && data[i] > data[i+1]) {
+            continue
+        }
+    }
+    return valleys_indices //make sure the return is outside of the for loop!!
+}
+let valley_indices = valleys(data)
+
+function peaks_and_valleys(peak_indices, valley_indices) { //referenced https://stackoverflow.com/questions/4856717/javascript-equivalent-of-pythons-zip-function
+    let zip_peaks_and_valleys = rows => rows[0].map((_,c) => rows.map(row => row[c]))
+    const peaks_valleysZipped = zip_peaks_and_valleys([peak_indices, valley_indices])
+    return peaks_valleysZipped
+}
+console.log(peaks_and_valleys(peak_indices,valley_indices))
+let result = peaks_and_valleys(peak_indices, valley_indices)
+alert(result)
