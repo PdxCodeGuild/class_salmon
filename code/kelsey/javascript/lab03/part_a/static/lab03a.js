7e232e6721b161a7b6bd277eb42ec442aca25f39
Kelsey <73902957+knizzle@users.noreply.github.com>
2021-07-01 15:49:26 -0700

    Update lab03a.js

--- code/kelsey/javascript/lab03/part_a/static/lab03a.js
+++ code/kelsey/javascript/lab03/part_a/static/lab03a.js
@@ -1,4 +1,4 @@
-// lab 01 Unit Converter
+// lab 01: Unit Converter
 
 // # Version 1
 // # Ask the user for the number of feet, and print out the equivalent distance in meters. Hint: 1 ft is 0.3048 m. So we can get the output in meters by multiplying the input distance by 0.3048. Below is some sample input/output.
