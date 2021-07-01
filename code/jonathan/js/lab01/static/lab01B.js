8c2cca87ff4f8f1e43895a92aa79e81a667af966
Jonathan Spannring <jonathan.spannring@gmail.com>
2021-07-01 16:26:32 -0700

    js labs

--- code/jonathan/js/lab01/static/lab01B.js
+++ code/jonathan/js/lab01/static/lab01B.js
@@ -7,32 +7,34 @@ let unitConverter = {
     'km': 1000,
 }
 
-let userValue = document.querySelector('#user_value');
-value_button.onclick = function() {
-    let newUserValue = userValue.value;
-    let newNumValue = parseFloat(newUserValue)
-    console.log(newNumValue)
-    return newNumValue
-}
-let userUnits = document.querySelector('#user_units');
-unit_button.onclick = function() {
-    let newUserUnits = userUnits.value;
-    console.log(newUserUnits)
-    return newUserUnits
-}
-let answerUnits = document.querySelector('#answer_units');
-answer_button.onclick = function() {
-    let newAnswerUnits = answerUnits.value;
-    console.log(newAnswerUnits)
-    return newAnswerUnits
-}
-let run_button = document.querySelector('#run_button')
-run_button.onclick = function(unitConverter, newUserUnits) {
-    console.log(unitConverter[newUserUnits])
 
-    //let userTotal = newNumValue * unitConverter[newUserUnits]
-    //let rawAnswer = userTotal / unitConverter[newAnswerUnits]
-    //let answer = rawAnswer.toFixed(2)
-    //console.log(answer)
-    //alert(`${userValue}${userUnits} comes out to ${answer}${answerUnits}.`)
-}
+
+let userValue = document.querySelector("#user-value")
+let startingUnits = document.querySelector("#starting-units")
+let endingUnits = document.querySelector("#ending-units")
+let runButton = document.querySelector("#run-button")
+
+runButton.addEventListener("click", function(event) {
+    event.preventDefault()
+    console.log(userValue.value)
+    function runButton() {
+        if (document.getElementById("in").checked) {
+            return ("in")
+        }
+        else if (document.getElementById("ft").checked) {
+            return ("ft")
+        }
+        else if (document.getElementById("yd").checked) {
+            return ("yd")
+        }
+        else if (document.getElementById("mi").checked) {
+            return ("mi")
+        }
+        else if (document.getElementById("m").checked) {
+            return ("m")
+        }
+        else if (document.getElementById("km").checked) {
+            return ("km")
+        }
+    }
+})
