a516740d5aa778fb3b62055db256c3bceb405c08
Jeremy Lamb <jeremy.c.lamb@gmail.com>
2021-07-02 16:56:18 -0700

    Lab1&2done

--- code/theotherjeremy/Javascript/Lab01/lab01redoblackjackpartB.js
+++ code/theotherjeremy/Javascript/Lab01/lab01redoblackjackpartB.js
@@ -1,14 +1,31 @@
-// let name_input = document.querySelector('#name_input');
-let firstCard = docment.querySelector('#firstCard')
-let run_bt = document.querySelector('#run_bt');
+let firstCard = document.querySelector('#firstCard')
+let secondCard = document.querySelector('#secondCard')
+let thirdCard = document.querySelector('#thirdCard')
 let output_div = document.querySelector('#output_div');
-run_bt.onclick = function() {
-    let first = firstCard.value;
-    output_div.innerText = 'Card 1: ' + first;
+let output_div2 = document.querySelector('#output_div2');
+let output_div3 = document.querySelector('#output_div3');
+let output_div4 = document.querySelector('#output_div4');
+let final = document.querySelector('#final');
+
+final.onclick = function(evt) {
+    let sumCards = 0
+    sumCards += cardVal[firstCard.value]  
+    sumCards += cardVal[secondCard.value]
+    sumCards += cardVal[thirdCard.value]
+    let advice 
+    if (sumCards < 17) {
+        advice = "You should hit.";
+    } else if (sumCards >= 17 && sumCards < 21) {
+        advice = "You should stay.";
+    } else if (sumCards == 21) {
+        advice = "BLACKJACK, BABY!!!";
+    } else if (sumCards > 21) {
+        advice = "Daww... BUST!";
+    }
+    output_div4.innerText = 'You got ' + sumCards + '...' + advice;
+
 }
-// prompt('What\'s your first card, playa?', 'first card');
-let secondCard = prompt('What\'s your second card, playa?', 'second card');
-let thirdCard = prompt('What\'s your third card, playa?', 'third card');
+
 const cardVal = {A: 1,
             2: 2,
             3: 3,
@@ -23,24 +40,3 @@ const cardVal = {A: 1,
             Q: 10,
             K: 10
             };
-let sumCards = (cardVal[firstCard] +
-             cardVal[secondCard] + cardVal[thirdCard]);
-             
-let advice 
-if (sumCards < 17) {
-    advice = "You should hit.";
-} else if (sumCards >= 17 && sumCards < 21) {
-    advice = "You should stay.";
-} else if (sumCards == 21) {
-    advice = "BLACKJACK, BABY!!!";
-} else if (sumCards > 21) {
-    advice = "Daww... BUST!";
-}
-alert(`You've got ${sumCards}. 
-${advice}`)
-
-
-//   let name = name_input.value;
-  //alert(name);
-  //output_div.innerHTML = '<b>Hello, ' + name + '!</b>';
-
