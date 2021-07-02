9e68308356f45f46b11af104ab2a9553d367179c
Nick Fry <nicholas.a.fry@gmail.com>
2021-07-02 15:12:58 +0000

    update lab01_b

--- code/nick/JavaScript/Lab01/templates/script_lab01_b.js
+++ code/nick/JavaScript/Lab01/templates/script_lab01_b.js
@@ -1,25 +1,46 @@
-let nums = [5, 0, 8, 3, 4, 1, 6]
-let list_nums = []
-let boolean = true
+let nums = document.getElementsByClassName("num")
+let addNumBtn = document.getElementById("add-num")
+let submitBtn = document.getElementById("submitBtn")
+let results = document.getElementById("results")
+
 console.log(nums)
-function isNumeric(num_or_done) {
-    return !isNaN(parseFloat(num_or_done))
+function isNumeric(nums) {
+    return !isNaN(parseFloat(nums))
 }
-while (boolean === true) {
-    let num_or_done = prompt("Enter a number or 'done'")
-    if (isNumeric(num_or_done) === true) {  //This is supposed to mean is not not a number for the parsefloat of the input https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
-        list_nums.push(parseFloat(num_or_done)) //needs type conversion for the string inputs
+addNumBtn.addEventListener("click", function(event) {
+    event.preventDefault() //This should replace the boolean while loop, I think. 
+    let list_nums = []
+    if (isNumeric(nums) === true) {  //This is supposed to mean is not not a number for the parsefloat of the input https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+        list_nums.push(parseFloat(nums)) //needs type conversion for the string inputs
         console.log(list_nums)
-        // return list_nums //cannot return unless this is a function
     } else {
         break
-        }
-}
-
+    }
 let sum = list_nums.reduce(function (a, b) {
     return a + b
   }, 0)
-console.log(sum);
+console.log(sum)
 let len = list_nums.length
 console.log(len)
-alert(`The average of your list values are ${sum/len}`)
+//Need to make a calculate average function when the user decides they are done adding all numbers
+addNumBtn.addEventListener("click", function(event) {
+    event.preventDefault()
+    let average = sum / len
+    results.append(average)
+
+// while (boolean === true) {
+//     let num_or_done = prompt("Enter a number or 'done'")
+//     if (isNumeric(num_or_done) === true) {  //This is supposed to mean is not not a number for the parsefloat of the input https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+//         list_nums.push(parseFloat(num_or_done)) //needs type conversion for the string inputs
+//         console.log(list_nums)
+//         // return list_nums //cannot return unless this is a function
+//     } else {
+//         break
+//         }
+// }
+
+
+// console.log(sum)
+// let len = list_nums.length
+// console.log(len)
+// alert(`The average of your list values are ${sum/len}`)
