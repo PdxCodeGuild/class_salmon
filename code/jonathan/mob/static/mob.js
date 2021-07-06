63492136feb6c79ab74f2d19ca7516e1765241e5
Kelsey <klsycanoy@yahoo.com>
2021-07-06 11:35:54 -0700

    ob to Jeremy

--- code/jonathan/mob/static/mob.js
+++ code/jonathan/mob/static/mob.js
@@ -1,10 +1,26 @@
 let submitButton = document.querySelector("#submit")
 let taskList = document.getElementById("tasks")
+let completed = document.getElementById("completed_tasks")
 
-submitButton.addEventListener("click", function(event) {
+submitButton.addEventListener("click", function (event) {
     event.preventDefault()
     let newTask = document.getElementById("new_task").value
     newerTask = document.createElement("li")
     newerTask.innerText = newTask
-    tasks.prepend(newerTask)
+    let removeBtn = document.createElement("button")
+    removeBtn.innerText = "-"
+    document.body.appendChild(removeBtn)
+    taskList.append(newerTask)
+    
+    // removeBtn.addEventListener("click", function (event) {
+    //     event.preventDefault()
+    //     this.previousSibling.remove()
+    //     this.remove()
+    // })
 })
+
+// function myFunction() {
+//     var str = "Hello World!";
+//     var result = str.strike();
+//     document.getElementById("demo").innerHTML = result;
+//   }
