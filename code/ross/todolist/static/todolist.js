549467a77ef34c26fb55be1ae5e3e508d658eb39
Midkniteskyz <rjwoolzee@gmail.com>
2021-07-06 16:54:58 -0700

    Update todolist.js

--- code/ross/todolist/static/todolist.js
+++ code/ross/todolist/static/todolist.js
@@ -1,18 +1,51 @@
 function newItem() {
+    var itemName = document.querySelector("#add-item");
     //var item = document.createElement("li")
-    let x = document.createElement("INPUT");
-    x.setAttribute("type", "checkbox");
-    x.setAttribute("name", "todo");
-    let itemName = document.getElementById("add-item").value
-    console.log(itemName)
-    x.setAttribute("value", itemName);
-    let text = document.createTextNode(itemName)
+    // x.setAttribute("id", itemName);
+    // x.setAttribute("class", itemName);
+    // console.log(itemName);
+    // x.setAttribute("value", itemName);
+    // let text = document.createTextNode(itemName);
     //item.appendChild(text)
-    document.getElementById("list1").appendChild(x)
-    console.log(x)
-    let y = document.createElement("label")
-    y.setAttribute("for", itemName)
-    y.innerHTML = itemName
-    document.getElementById("list1").appendChild(y)
-    console.log(y)
+    var firstList = document.querySelector("#list1");
+    var listItem = document.createElement("li");
+    listItem.innerHTML = itemName.value;
+    firstList.appendChild(listItem);
+    // var y = document.createElement("label");
+    // y.setAttribute("for", itemName);
+    // y.setAttribute("class", itemName);
+    // y.setAttribute("id", itemName);
+    // y.innerHTML = itemName;
+    // var something2 = document.getElementById("#list1").appendChild(y);
+    // console.log(y);
+    var completeButton  = document.createElement("button");
+    completeButton.innerHTML = "complete";
+    completeButton.setAttribute("onclick", "completed()");
+    console.log(completeButton);
+    firstList.appendChild(completeButton);
+    firstList.appendChild(itemName.value);
+    var removeButton  = document.createElement("button");
+    removeButton.innerHTML = "remove";
+    removeButton.setAttribute("onclick", "removeStuff()");
+    console.log(removeButton);
+    firstList.appendChild(removeButton);
+    // let br = document.createElement("br");
+    // firstList.appendChild(br);
+}
+
+function completed() {
+    event.preventDefault();
+
+}
+
+
+function removeStuff() {
+
+    event.preventDefault();
+    let itemVal = document.querySelector('input[type="checkbox"]:checked').value
+    console.log(itemVal)
+    let itemClass = document.getElementsByClassName(itemVal);
+    console.log(itemClass)
+    // itemClass.remove()
+    
 }
