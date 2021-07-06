911cc55c0de345acbcecb67d16a566a2ad40102c
code610 <82845353+code610@users.noreply.github.com>
2021-07-06 12:32:57 -0700

    updated todolist

--- code/ross/todolist/static/todolist.js
+++ code/ross/todolist/static/todolist.js
@@ -1,6 +1,18 @@
 function newItem() {
-    var item = document.createElement("li")
-    var itemName = document.getElementById("add-item").value
-    var text = document.createTextNode(itemName)
-    item.appendChild(text)
+    //var item = document.createElement("li")
+    let x = document.createElement("INPUT");
+    x.setAttribute("type", "checkbox");
+    x.setAttribute("name", "todo");
+    let itemName = document.getElementById("add-item").value
+    console.log(itemName)
+    x.setAttribute("value", itemName);
+    let text = document.createTextNode(itemName)
+    //item.appendChild(text)
+    document.getElementById("list1").appendChild(x)
+    console.log(x)
+    let y = document.createElement("label")
+    y.setAttribute("for", itemName)
+    y.innerHTML = itemName
+    document.getElementById("list1").appendChild(y)
+    console.log(y)
 }
