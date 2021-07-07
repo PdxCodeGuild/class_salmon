872327278d84bcea02d1308439376ce251078d4c
Jonathan Spannring <jonathan.spannring@gmail.com>
2021-07-07 10:56:15 -0700

    lab01_02B Done

--- code/jonathan/js/lab03/static/lab03B.js
+++ code/jonathan/js/lab03/static/lab03B.js
@@ -1,35 +1,9 @@
-function greaterThanNine(num) {
-    if (num > 9) {
-        num -= 9
-    }
-    else {   
-        num -= 0
-    }
-    return num
-}
-
-let runButton = document.querySelector("#run-button")
+let userInput = document.querySelector("#user-input")
+let runButton = document.querySelector("#run-btn")
 
 runButton.addEventListener("click", function(event) {
     event.preventDefault()
-    let userCC = document.getElementById("textarea")
-    let newUserCC = parseFloat(userCC.value)
-    console.log(newUserCC)
-    let userCCList = newUserCC.split("")
-    let checkDigit = newUserCC.pop()
-    let reverseUserCCList = userCCList.reverse()
-    for (let i = 0; i < reverseUserCCList.length; i += 2) {
-    reverseUserCCList[i] *= 2;}
-    let doubleList = reverseUserCCList
-    let newList = doubleList.map(greaterThanNine)
-    let sumNum = newList.reduce(function(a, b){
-        return a + b;
-    }, 0)
-    let lastCheck = sumNum % 10
-    if (lastCheck == checkDigit) {
-        alert("Valid.")
-    } 
-    else {
-        alert("Sorry, that is not valid.")
-    }
+    let ccF = userInput.value
+    let ccS = ccF.split("")
+    console.log(ccS)
 })
