a24a0b8d77c3fdcc68f6f32e3db7770620909cb5
Kelsey <klsycanoy@yahoo.com>
2021-07-07 17:31:09 -0700

    lab01b js

--- code/kelsey/javascript/lab01/part_b/static/script.js
+++ code/kelsey/javascript/lab01/part_b/static/script.js
@@ -1,27 +1,35 @@
-let firstCard = document.getElementById("first")
-let secondCard = document.getElementById("second")
-let thirdCard = document.getElementById("third")
+// 1. queries - get pieces from html
+// 2. input from user
+// 3. formula => advice
+// 4. return advice
 
-const pointValue = {
-    A: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8,
-    9: 9,
-    10: 10,
-    J: 10,
-    Q: 10,
-    K: 10
+let form = document.querySelector("#forms")
+let firstCard = document.querySelector("#first")
+let secondCard = document.querySelector("#second")
+let thirdCard = document.querySelector("#third")
+let btn = document.querySelector("#submit")
+let advice = document.querySelector("#advice")
+
+function totalPoints(firstCard, secondCard, thirdCard) {
+    let total = parseInt(firstCard.value) + parseInt(secondCard.value) + parseInt(thirdCard.value)
+    return total
 }
 
-let cardOne = parseInt(pointValue[firstCard])
-let cardTwo = parseInt(pointValue[secondCard])
-let cardThree = parseInt(pointValue[thirdCard])
-
-let totalPoints = parseInt(cardOne + cardTwo + cardThree)
-
-onsubmit
+btn.addEventListener("click", function(event) {
+    event.preventDefault()
+    let total = totalPoints(firstCard, secondCard, thirdCard)
+    let newAdvice = document.createElement("li")
+    let horizontalRule = document.createElement("hr")
+    newAdvice.innerText = `Your total is: ${total}.`
+    if (total < 17) {
+        advice.prepend('Hit!')
+    } else if (21 > total && total >= 17) {
+        advice.prepend('Stay!')
+    } else if (total === 21) {
+        advice.prepend('Blackjack!')
+    } else if (total > 21) {
+        advice.prepend('Already Busted.')
+    }
+    advice.prepend(newAdvice)
+    advice.prepend(horizontalRule)
+})
