b2915ab1c614f2df6c1755368ac6f99bd64e3c98
Nick Fry <nicholas.a.fry@gmail.com>
2021-07-08 23:30:39 +0000

    nothing much

--- code/nick/JavaScript/Vue/app.js
+++ code/nick/JavaScript/Vue/app.js
@@ -1,12 +1,16 @@
 let vm = new Vue({
     el: '#app',
     data: {
-        tasklist: []
+            completed_tasklist: [],
+            input_task: '',
+            incomplete_tasklist: [],
     },
     methods: {
-        task: function() {
-            console.log(this.tasklist);
+        addToList: function() {
+            let input = this.input_task
+            console.log(input)
+            this.incomplete_tasklist.push(input)
+            console.log(this.incomplete_tasklist, "incomplete_tasklist")
         }
-
-    }
+    }        
 })
