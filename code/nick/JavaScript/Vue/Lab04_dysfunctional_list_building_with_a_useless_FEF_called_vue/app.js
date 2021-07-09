1c6be5de385f01c9c547acc7e2d77884e049d494
Nick Fry <nicholas.a.fry@gmail.com>
2021-07-09 13:10:09 +0000

    lab04 vue update

--- code/nick/JavaScript/Vue/Lab04_dysfunctional_list_building_with_a_useless_FEF_called_vue/app.js
+++ code/nick/JavaScript/Vue/Lab04_dysfunctional_list_building_with_a_useless_FEF_called_vue/app.js
@@ -3,6 +3,7 @@ let vm = new Vue({
     data: {
             completed_tasklist: [],
             input_task: '',
+            selected_task: '',
             incomplete_tasklist: [],
     },
     methods: {
@@ -11,6 +12,14 @@ let vm = new Vue({
             console.log(input)
             this.incomplete_tasklist.push(input)
             console.log(this.incomplete_tasklist, "incomplete_tasklist")
+        },
+        addToCompleted: function() {
+            console.log(this.selected_task, "selected_task")
+            let li = this.selected_task
+            console.log(li)
+            this.completed_tasklist.push(li)
+            console.log(this.completed_tasklist, "completed tasklist")
+            this.incomplete_tasklist.remove(selected_task)
         }
     }        
 })
