fcfba22d565ad63ab6ed2d4024d8250b5ffd5090
Nick Fry <nicholas.a.fry@gmail.com>
2021-07-15 23:30:16 +0000

    updates to JS minicapstone

--- code/nick/JavaScript/Vue/mini-capstone/app.js
+++ code/nick/JavaScript/Vue/mini-capstone/app.js
@@ -22,6 +22,36 @@
 //         },
 //     },
 // })
+// Vue.component('rates-layout', , {
+//     // data: function() {
+//     //     return {quoteResponse:{}}
+//     // },
+//     template: '\
+//       <li>\
+//         The residential rate in {{ cityName }}is ${{ residentialRate }} per kWh.\
+//       </li>\
+//     ',
+//     props: ['cityName', 'residentialRate'],
+//   })
+// Vue.component('rate-items', {
+//     template: '\
+//     <template>\
+//       <ul>\
+//         <p>The residential rate for electricity is ${{ residentialRate }} per kWh</p>\
+//         <p>The commercial rate for electricity is ${{ commercialRate }} per kWh</p>\
+//         <p>The industrial rate for electricity is ${{ industrialRate }} per kWh</p>\
+//         <h3 class="subtitle">Operating companies in this service area include:</h3>\
+//         <li \
+//         v-for="index in utilityInfo"\
+//         v-bind:key="index.id"\
+//         >{{ index.utility_name }}</li>\
+//       </ul>\
+//       <img src"https://api.mapbox.com/styles/v1/mapbox/streets-v11/statichttps://api.mapbox.com/styles/v1/mapbox/dark-v10/static/{{ latlonResponse.latt }},{{ latlonResponse.longt }},10,0/275x200?access_token=pk.eyJ1IjoiYXplcm1hbiIsImEiOiJja3I1YXgzYmszM2o2Mm9xcGd6NThkZTZjIn0.O3OKKsfIG0ZyyF-LvNdu_g"></img>\
+//     </template>\
+//     ',
+//     props: ['residentialRate', 'commercialRate', 'industrialRate', 'utilityInfo', 'latlonResponse.latt', 'latlonResponse.longt']
+//   })
+
 let vm = new Vue({
     el: '#app',
     data: {
@@ -35,6 +65,8 @@ let vm = new Vue({
         commercialRate: [],
         residentialRate: [],
         industrialRate: [],
+        variableCheck: false,
+        mapUrl: ""
         // pageInput: 1,
     },
     methods: {
@@ -51,6 +83,7 @@ let vm = new Vue({
             }).then(response => {
                 this.latlonResponse = response.data
                 this.cityName = response.data.standard.city
+                this.variableCheck = true
                 console.log(response.data.standard.city)
                 this.loadRates()
             }
@@ -75,10 +108,12 @@ let vm = new Vue({
                 // url: `https://favqs.com/api/quotes/?filter=${filter}&type=${type}`,
             }).then(response => {this.rateResponse = response.data,
                 this.utilityInfo = response.data.outputs.utility_info,
-                this.commercialRate = response.data.outputs.commericial,
+                this.commercialRate = response.data.outputs.commercial,
                 this.residentialRate = response.data.outputs.residential,
                 this.industrialRate = response.data.outputs.industrial,
-                console.log(response.data.outputs.utility_info)
+                this.mapUrl = `https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/static/${this.latlonResponse.longt},${this.latlonResponse.latt},8.75,0/275x200?access_token=pk.eyJ1IjoiYXplcm1hbiIsImEiOiJja3I1YXgzYmszM2o2Mm9xcGd6NThkZTZjIn0.O3OKKsfIG0ZyyF-LvNdu_g`,
+                console.log(String(this.mapUrl))
+                console.log(response.data.outputs.utility_info),
                 console.log(response.data.outputs),
                 console.log(this.latlonResponse)}
             )
