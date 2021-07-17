1b40c53fa4d9e15e781582ef98c480962d5b9cad
Kelsey <klsycanoy@yahoo.com>
2021-07-17 14:39:51 -0700

    lab05 progress

--- code/kelsey/javascript/lab05/site.js
+++ code/kelsey/javascript/lab05/site.js
@@ -6,26 +6,47 @@
 // When the page first loads, show the user a set of completely random quotes.
 // You must have at least one Vue component in your app.
 
-Vue.component('', {
-    data: function() {
-        return
-    }, 
-    template: ''
+Vue.component('show-results', {
+    props: ["results"],
+    data: function () {
+        return {
+            results: this.results
+        }
+    },
+    template: `<div v-for="result in results">{{ result.body }}</div>`
 })
+
 const vm = new Vue({
     el: '#app',
+    // components: {
+    //     'show-results': show-results
+    // },
     data: {
-        results: ''
+        results: null,
+        searchSelect: '',
+        searchInput: '',
+
     },
-    search() {
-        axios({
-            method: 'get',
-            url: 'https://favqs.com/api/quotes/',
-            headers: {
-                "Authorization": `Token token="75781e1e8edbf2eb68848384abbbd2bb"`
-            }.then(response => {
+    methods: {
+        search() {
+
+            axios({
+                method: 'get',
+                url: 'https://favqs.com/api/quotes/',
+                headers: {
+                    "Authorization": `Token token="75781e1e8edbf2eb68848384abbbd2bb"`
+                },
+                params: {
+                    filter: this.searchInput,
+                    type: this.searchSelect
+
+                }
+            }).then(response => {
                 console.log(response)
+                this.results = response.data.quotes
             })
-        })
+        },
+        
     }
+
 })
