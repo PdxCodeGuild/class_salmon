1a2cd7f15dcf315ca7b252380463d44459f47084
Nick Fry <nicholas.a.fry@gmail.com>
2021-07-19 18:03:46 +0000

    FizzBuzz

--- code/nick/python/FizzBuzz/fizzbuzz.py
+++ code/nick/python/FizzBuzz/fizzbuzz.py
@@ -2,11 +2,11 @@
 # But for multiples of three print “Fizz” instead of the number 
 # and for the multiples of five print “Buzz”. 
 # For numbers which are multiples of both three and five print “FizzBuzz”.
-for nums in range(1,101):
-    if nums%3==0 and nums%5==0:
-        nums="FizzBuzz"    
-    elif nums%3==0:
-        nums="fizz"
-    elif nums%5==0:
-        nums="buzz"
-    print(nums)
+for num in range(1,101):
+    if num%3==0 and num%5==0:
+        num="FizzBuzz"    
+    elif num%3==0:
+        num="fizz"
+    elif num%5==0:
+        num="buzz"
+    print(num)
