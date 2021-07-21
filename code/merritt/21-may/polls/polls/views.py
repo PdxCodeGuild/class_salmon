c6035c76218f2f5e64280d26ff75b787cb04f281
Merritt Lawrenson <merrittlawrenson@gmail.com>
2021-07-21 10:10:43 -0700

    so far

--- code/merritt/21-may/polls/polls/views.py
+++ code/merritt/21-may/polls/polls/views.py
@@ -16,8 +16,8 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = f"You're looking at the results of question {question_id}."
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
