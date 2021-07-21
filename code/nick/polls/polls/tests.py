6c4b35effd3519724254fd7df4545bf7d32387b3
Nick Fry <nicholas.a.fry@gmail.com>
2021-07-21 23:38:15 +0000

    in class django demonstration update

--- code/nick/polls/polls/tests.py
+++ code/nick/polls/polls/tests.py
@@ -1,3 +1,74 @@
+import datetime
+from django.http import response
+
+
 from django.test import TestCase
+from django.urls.base import reverse
+from django.utils import timezone
+
+from .models import Question
 
 # Create your tests here.
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self): #anything with the first word test will run when the class is called
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")#user gets a message that no polls are available if none are available
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_pass_question(self):
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question])
+    
+    def test_future_question(self):
+        question = create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        past_question = create_question(question_text="Past Question.", days=-30)
+        future_question = create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [past_question])
+    
+    def test_two_past_questions(self):
+        question1 = create_question(question_text='Past question1.', days=-30)
+        question2 = create_question(question_text='Past question2.', days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(response.context['latest_question_list'], [question2, question1])#question2 comes first in the list because it is newer
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse('polls:detail', args=[future_question.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+    
+    def test_past_question(self):
+        past_question = create_question(question_text='Past question.', days=-5)
+        url = reverse('polls:detail', args=[past_question.id])
+        response = self.client.get(url)
+        # print(response.content)
+        self.assertContains(response, past_question.question_text)
--- code/nick/polls/polls/views.py
+++ code/nick/polls/polls/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 
 from .models import Choice, Question
 # Create your views here.
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]#descending pub_date
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]#descending pub_date
     context = {
         'latest_question_list':latest_question_list
     }
@@ -16,13 +17,13 @@ def index(request):
 #vote
 #results
 def detail(request, question_id):#as many pages as questions in DB
-    question= get_object_or_404(Question, pk=question_id)
+    question= get_object_or_404(Question.objects.filter(pub_date__lte=timezone.now()), pk=question_id)#lte is less than or equal to
     #pk stands for primary key, the unique non-repeating key is for the data
     return render(request, 'polls/detail.html', {'question': question})
-
+#on the view level the detail function and the results function are doing the same thing. The only difference to us is the display of options versus results.
 def results(request, question_id):
-    response = f'you are looking at the results of question {question_id}'
-    return HttpResponse(response)
+    question= get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -33,6 +34,6 @@ def vote(request, question_id):
             'question':question,
             'error_message': "You did not select a choice.",
         })
-    selected_choice.votes +=1
+    selected_choice.votes += 1
     selected_choice.save()
     return HttpResponseRedirect(reverse('polls:results', args=[question.id]))#a tuple with one item in it needs a trailing comma
