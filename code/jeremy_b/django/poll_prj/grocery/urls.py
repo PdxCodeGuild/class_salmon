be5c8818b32da739cc4c38eaa29f7f21d03e3e97
b34systems <Engine#21>
2021-07-23 20:40:23 -0700

    Works: add lists & list items, change name of list works.  Error on change list status from active to inactive.  Believe problem is with value returned by checkbox. Delete list or list item not implemented yet.

--- code/jeremy_b/django/poll_prj/grocery/urls.py
+++ code/jeremy_b/django/poll_prj/grocery/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('<int:list_id>/detail/', views.list_detail, name='list_detail'),
     path('add_list', views.add_list, name='add_list'),
     path('edit_list', views.edit_list, name='edit_list'),
-    path('request_complete', views.request_complete, name='request_complete')
+    path('request_complete', views.request_complete, name='request_complete'),
+    path('add_list_item/<int:pk>', views.add_list_item, name='add_list_item'),
+    path('edit_list/<int:list_id>', views.edit_list, name='edit_list'),
 ]
--- code/jeremy_b/django/poll_prj/grocery/views.py
+++ code/jeremy_b/django/poll_prj/grocery/views.py
@@ -13,14 +13,19 @@ def index(request):
 def list_detail(request, list_id):
     list_items = ListItem.objects.filter(list_id=list_id).filter(is_complete=False)
     item_list_len = len(list_items)
-    return render(request, 'grocery/list_detail.html', {'list_items': list_items, 'item_list_len': item_list_len })
+    list_name = List.objects.filter(pk=list_id).get().list_name
+    context = {
+        'list_items': list_items,
+        'item_list_len': item_list_len,
+        'list_id': list_id,
+        'list_name': list_name
+    }
+    return render(request, 'grocery/list_detail.html', context)
 
 
 def add_list(request):
     if request.POST:
         new_list_name = request.POST
-        print(new_list_name)
-        print(new_list_name['list_name'])
         new_list = List(list_name=new_list_name['list_name'])
         new_list.save()
         return HttpResponseRedirect(reverse('grocery:request_complete'))
@@ -28,11 +33,35 @@ def add_list(request):
         return render(request, 'grocery/add_list.html')
 
 
-def add_list_item(request):
-    pass
+def add_list_item(request, pk):
+    if request.POST:
+        list_id = List.objects.filter(pk=pk).get().id
+        new_item_name = request.POST
+        new_item = ListItem(list_item_text=new_item_name['list_item_name'], list_id=list_id)
+        new_item.save()
+        return list_detail(request, pk)
+    else:
+        list_name = List.objects.filter(pk=pk).get().list_name
+        return render(request, 'grocery/add_list_item.html', {'list_id': pk, 'list_name': list_name})
 
-def edit_list(request):
-    return render(request, 'grocery/edit_list.html')
+def edit_list(request, list_id):
+    q = List.objects.filter(pk=list_id).get()
+    if request.POST:
+        print(request.POST['is_active'])
+        if request.POST['is_active'] == 'on':
+            q.is_active = True
+        else:
+            q.is_active = False
+        q.list_name = request.POST['list_name']
+        q.save()
+        return list_detail(request, list_id)
+    else:
+        context = {
+            'list_id': q.pk,
+            'list_name': q.list_name,
+            'is_active': q.is_active
+        }
+        return render(request, 'grocery/edit_list.html', context)
 
 def edit_list_item(request):
     pass
