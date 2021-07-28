7fe626e167af2ed069523392b75cf9f197e3d667
Kelsey <klsycanoy@yahoo.com>
2021-07-28 08:20:06 -0700

    lab04 done, lab05 in progress

--- code/kelsey/javascript/lab05/site.js
+++ code/kelsey/javascript/lab05/site.js
@@ -1,11 +1,3 @@
-// Requirements:
-
-// Your app must use Vue to fetch data and interact with the results.
-// Let the user enter a search term and select whether to search by keyword, author, or tag.
-// Implement pagination buttons when the search returns more than 25 quotes.
-// When the page first loads, show the user a set of completely random quotes.
-// You must have at least one Vue component in your app.
-
 Vue.component('show-results', {
     props: ["results"],
     data: function () {
@@ -21,19 +13,27 @@ const vm = new Vue({
     data: {
         results: null,
         searchSelect: '',
-        searchInput: '',
-
+        searchInput: ''
     },
     methods: {
         loadQuotes() {
-            
+            axios({
+                method: 'get',
+                url: 'https://favqs.com/api/quotes/',
+                headers: {
+                    "Authorization": `Token token=${apiKey}`
+                }
+            }).then(response => {
+                console.log(response)
+                this.results = response.data.quotes
+            })
         },
         search() {
             axios({
                 method: 'get',
                 url: 'https://favqs.com/api/quotes/',
                 headers: {
-                    "Authorization": `Token token="75781e1e8edbf2eb68848384abbbd2bb"`
+                    "Authorization": `Token token=${apiKey}`
                 },
                 params: {
                     filter: this.searchInput,
@@ -44,8 +44,14 @@ const vm = new Vue({
                 this.results = response.data.quotes
             })
         },
-        created: function() {
+        created() {
             this.loadQuotes()
+        },
+        nextPg() {
+
+        },
+        previousPg() {
+
         }
 
     }
