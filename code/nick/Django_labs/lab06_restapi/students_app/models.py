8ff063cd9ccf33d2e0f4ff20e8ce65d368b43ae4
Nick Fry <nicholas.a.fry@gmail.com>
2021-08-03 15:30:32 +0000

    lab06_restapi update

--- code/nick/Django_labs/lab06_restapi/students_app/models.py
+++ code/nick/Django_labs/lab06_restapi/students_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Students(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    cohort = models.CharField(max_length=6)#call corhorts by year and two digit number
+    favorite_topic = models.CharField(max_length=120)
+    favorite_teacher = models.CharField(max_length=120)
+    capstone_url = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.first_name
