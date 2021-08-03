a33963ec858569453f9b4d1e32e942546ac70672
Nick Fry <nicholas.a.fry@gmail.com>
2021-08-03 21:30:24 +0000

    lab06 working, allow any mode

--- code/nick/Django_labs/lab06_restapi/restapi_app/views.py
+++ code/nick/Django_labs/lab06_restapi/restapi_app/views.py
@@ -3,12 +3,12 @@ from django.contrib.auth import get_user_model
 
 from students_app.models import Students
 from .serializers import StudentsSerializer, UserSerializer
-from .permissions import IsAuthorOrReadOnly
+from .permissions import AllowAny
 
 class StudentsViewSet(viewsets.ModelViewSet):
     queryset = Students.objects.all()
     serializer_class = StudentsSerializer
-    permission_classes = [IsAuthorOrReadOnly]
+    permission_classes = [AllowAny]
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = get_user_model().objects.all()
