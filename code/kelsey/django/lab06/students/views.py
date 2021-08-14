2a4bb5b16b1b08bdb170898154968f7be6aed34d
Kelsey <klsycanoy@yahoo.com>
2021-08-14 15:00:35 -0700

    Vue methods still off

--- code/kelsey/django/lab06/students/views.py
+++ code/kelsey/django/lab06/students/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from .models import Student
 
 def home(request):
-    return render(request, 'student_list.html')
+    return render(request, 'students/student_list.html')
