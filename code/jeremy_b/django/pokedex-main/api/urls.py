c4486d2c42698e53b7f1f7566c3c53632835f075
n8jja <jeremy@n8jja.com>
2021-09-09 10:03:17 -0700

    Lab 7 complete.

--- code/jeremy_b/django/pokedex-main/api/urls.py
+++ code/jeremy_b/django/pokedex-main/api/urls.py
@@ -11,8 +11,10 @@ router.register('pokemon', PokemonViewSet, basename="pokemon")
 router.register('type', TypeViewSet, basename="types")
 router.register('users', UserViewSet, basename="users")
 
-urlpatterns = [
+urlpatterns = urlpatterns = [
     path('api/v1/', include(router.urls)),
     path('api/v1/current_user/', UserView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+
--- code/jeremy_b/django/pokedex-main/static/pokedex.js
+++ code/jeremy_b/django/pokedex-main/static/pokedex.js
@@ -14,27 +14,93 @@ let app = new Vue({
         user: '',
         pokemon: [],
         caught: [],
+        caughtList: [],
         csrf_token: document.querySelector("input[name=csrfmiddlewaretoken]").value,
     },
     methods:{
         loadPokemon: function() {
-            axios.get({url: '/api/v1/pokemon'}).then(response => {
+            axios({
+                method: 'GET',
+                url: '/api/v1/pokemon'
+            }).then(response => {
                 this.pokemon = response.data;
-                console.log(this.pokemon);
             })
+        },
+        catchPokemon: function(pokemon_id){
+            console.log(pokemon_id);
+            let new_list = [];
+            for (i = 0; i < this.caught.length; i++){
+                new_list.push(this.caught[i])
+            }
+            new_list.push(pokemon_id)
+            axios({
+                method: 'patch',
+                url: `/api/v1/users/${this.user.id}/`,
+                headers: {
+                    'X-CSRFToken': this.csrf_token
+                },
+                data: {
+                    'caught': new_list
+                }
+            }).then(response => {
+                this.getUpdatedCaughtList();
 
-
+                this.loadPokemon();
+            });
+        },
+        releasePokemon: function(pokemon_id){
+            let new_list = [];
+            for (i = 0; i < this.caught.length; i++){
+                new_list.push(this.caught[i])
+            }
+            let index = new_list.indexOf(pokemon_id)
+            console.log(index)
+            new_list.splice(index, 1)
+            console.log(new_list)
+            axios({
+                method: 'patch',
+                url: `/api/v1/users/${this.user.id}/`,
+                headers: {
+                    'X-CSRFToken': this.csrf_token
+                },
+                data: {
+                    'caught': new_list
+                }
+            }).then(response => {
+                this.getUpdatedCaughtList();
+                this.loadPokemon();
+            });
+        },
+        getUpdatedCaughtList: function (){
+            this.loadPokemon();
+            axios({
+                method: 'GET',
+                url: '/api/v1/current_user'
+            }).then(response => {
+                this.user = response.data;
+                this.caught = this.user.caught;
+            })
+        },
+        makeCaughtList: function() {
+            for (poke in this.pokemon){
+                if (poke.id + 1 in this.caught){
+                    this.caughtList.push(poke)
+                }
+            }
         }
     },
     mounted: function() {
         this.$nextTick(function() {
             this.loadPokemon();
-            axios.get({url: '/api/v1/current_user'}).then(response => {
+            axios({
+                method: 'GET',
+                url: '/api/v1/current_user'
+            }).then(response => {
                 this.user = response.data;
                 this.caught = this.user.caught;
             })
         })
-    }
+    },
 })
 
 // Other JS.
